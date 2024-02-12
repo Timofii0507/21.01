90e282e6ddac961f2c6699e48589b0668ed0005f
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-12 18:45:29 +0200

    Завдання  з  дз

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-#include "dai.h"
+#include <iostream>
 #include <Windows.h>
+#include "dai.h"
 
-using namespace std;
-
-
-int main() {
+int main()
+{
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     BinarySearchTree bst;
 
-    ifstream inputFile("DAI.txt");
+    std::ifstream inputFile("DAI.txt");
     if (inputFile.is_open()) {
         bst.loadFromFile(inputFile);
         inputFile.close();
